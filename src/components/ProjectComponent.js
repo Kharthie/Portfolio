fa7c2d977ef015c6b99148697349f77d65527fb9
kharthie <kharthieramakrishnan.com>
2022-04-29 23:32:00 +0530

    correction3

--- src/components/ProjectComponent.js
+++ src/components/ProjectComponent.js
@@ -25,8 +25,6 @@ class Projects extends Component {
     window.open(projObj2.link2, "_blank");
   }
 
-
-
   openProjectsURL() {
     window.open(this.props.details.profile_links.github.link, "_blank");
   }
@@ -38,8 +36,6 @@ class Projects extends Component {
   openProjectsURL() {
     window.open(this.props.details.profile_links.github.link2, "_blank");
   }
-  
-
 
   render() {
     const projects = this.props.details.projects.map((project) => {
@@ -51,32 +47,35 @@ class Projects extends Component {
               alt={project.title}
               width="22%"
               height="75%"
-              style={{ margin: "auto"  }}
+              style={{ margin: "auto" }}
             />
           </div>
           <div className="col-12 col-sm-6">
             <h4>{project.title}</h4>
             <p className="text-justify">{project.description.para1}</p>
             <p className="text-justify">{project.description.para2}</p>
-             <button
+            <button
               className="view-projects view-projects--specs"
-              style={{ borderRadius: "5px" , color:"black"}}
+              style={{ borderRadius: "5px", color: "black" }}
               onClick={() => this.onProjectClicked(project.id)}
             >
               View Project
-            </button> 
-            
-             <button className="view-projects view-projects--specs"
-              style={{ borderRadius: "5px" , color:"black"}}
+            </button>
+
+            <button
+              className="view-projects view-projects--specs"
+              style={{ borderRadius: "5px", color: "black" }}
               onClick={() => this.onProjectClicked1(project.id)}
-              >
-                Front-end
-                </button> 
-                <button className="view-projects view-projects--specs"
-              style={{ borderRadius: "5px" , color:"black"}}
+            >
+              Front-end
+            </button>
+            <button
+              className="view-projects view-projects--specs"
+              style={{ borderRadius: "5px", color: "black" }}
               onClick={() => this.onProjectClicked2(project.id)}
-              >Back-end</button> 
-           
+            >
+              Back-end
+            </button>
           </div>
         </div>
       );
@@ -84,43 +83,37 @@ class Projects extends Component {
 
     return (
       <>
+        <section id="projects">
+          <div className="container">
+            <h1 className="text-center1">P r o j e c t s :</h1>
 
-       <section id="projects">
-        <div className="container">
-          <h1 className="text-center1">P r o j e c t s :</h1>
-        
-
-          {projects}
-          <div className="text-center">
-            <div
-              style={{ marginTop: "30px", borderRadius: "5px",backgroundColor:"black" }}
-              className="view-projects view-projects--specs"
-            //  onClick={() =>
-            //   this.openLink(this.props.details.profile_links.github)
-            // }
-            >
-             <h4 className="more">For More Projects Visit My GitHub.</h4>
-            </div> 
-          
-
-
+            {projects}
+            <div className="text-center">
+              <div
+                style={{
+                  marginTop: "30px",
+                  borderRadius: "5px",
+                  backgroundColor: "black",
+                }}
+                className="view-projects view-projects--specs"
+                //  onClick={() =>
+                //   this.openLink(this.props.details.profile_links.github)
+                // }
+              >
+                <h4 className="more">For More Projects Visit My GitHub.</h4>
+              </div>
+            </div>
+            <img
+              style={{
+                width: "150px",
+                margin: "10px",
+                marginLeft: "480px",
+              }}
+              src="https://thumbs.gfycat.com/HeavenlyCoordinatedGermanwirehairedpointer-size_restricted.gif"
+            ></img>
+            {/* <div class="progress-4"></div> */}
           </div>
-          <img 
-          style={{
-            width:"150px",
-            margin: "10px",
-            marginLeft: "480px",
-          }}
-          src="https://thumbs.gfycat.com/HeavenlyCoordinatedGermanwirehairedpointer-size_restricted.gif"></img>
-          {/* <div class="progress-4"></div> */}
-        </div>
-      </section> 
-
- 
-          
-
-
-
+        </section>
       </>
     );
   }
